e6837b71e77cee706e609fa0f58b5a15af6045b2
Alex Ballera <alexballera@gmail.com>
2020-10-20 14:58:33 -0300

    Agregado compoenente angular element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
+import { AngularElementComponent } from './components/angular-element/angular-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularElementComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent],
+  entryComponents: [AngularElementComponent]
 })
-export class AppModule { }
+
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) {}
+  ngDoBootstrap(): void {
+    customElements.define(
+      'angular-element',
+      createCustomElement(AngularElementComponent, { injector: this.injector }),
+    );
+  }
+}
